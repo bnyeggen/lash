5b19f9a23d7dfb59e66652a5a65b26755a8cb831
Bryce Nyeggen <bnyeggen@gmail.com>
2014-08-03 16:36:10 -0700

    Docs

--- src/main/java/com/nyeggen/lash/util/InsertHelper.java
+++ src/main/java/com/nyeggen/lash/util/InsertHelper.java
@@ -3,6 +3,9 @@ package com.nyeggen.lash.util;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
+//This is used by tests only - it should be part of the testing
+//packages, but we ran into some issues with Maven / Eclipse classpath
+//conflicts.
 public class InsertHelper {
 	public static final byte[] longToBytes(long i) {
 		final ByteBuffer buf = ByteBuffer.allocate(8);
